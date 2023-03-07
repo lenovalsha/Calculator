6e86940b65d3e71c1b067a739462cd9d5a8f5b9a
lenovalsha <lenovalsha@gmail.com>
2023-03-07 15:06:05 -0700

    got the logic added but not done

--- index.js
+++ index.js
@@ -1,7 +1,70 @@
 var button = document.querySelector('button');
 var box = document.getElementById('box');
+var buttonArr = document.querySelectorAll('button');
+
+var hasOperation;
+var operationVal = "";
+var x = "";
+var y ="";
 
 button.onclick = function ChangeMe(){
     console.log('clicked');
     box.style.backgroundColor = 'green';
 }
+buttonArr.forEach(btn => {
+    var val = btn.innerHTML;
+    btn.onclick = function Change(){
+        
+        if(!hasOperation && val !== "=")
+        {
+            x += val.toString();
+            console.log(x);
+            btn.style.backgroundColor = "yellow";
+
+        }else if(hasOperation && val !== "=")
+        {
+            y += val.toString();
+            console.log(y);
+            btn.style.backgroundColor = "red";
+
+        }else if(val === "+"||val === "-"||val === "*"||val === "/")
+        {
+            hasOperation = true;
+            operationVal = val;
+        }else if(hasOperation && val =="=")
+        {
+            switch(operationVal)
+            {
+                case "+":
+                    x+=y;
+                    break;
+                case "-":
+                    x-=y;
+                    break;
+                case "*":
+                    x*=y;
+                    break;
+                case "/":
+                    x/=y;
+                    break;
+
+            }
+            console.log(x);
+        }
+    }
+    
+});
+
+
+
+
+
+
+
+// ANOTHER WAY OF DOING THIS USING FOR LOOP
+// for(let i =0;i<buttonArr.length;i++)
+// {
+//     buttonArr[i].onclick = function Calc(){
+//     console.log(buttonArr[i].innerHTML);
+// }
+// }
